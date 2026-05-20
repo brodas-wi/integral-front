9d4fb6e7ce098cd267dd62c9132ebf3d57f8f178
Steve <brodas@web-informatica.com>
2026-05-20 16:53:04 -0600

    build: rebuild frontend assets with updated CSS bundle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
-    safelist: [],
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+    safelist: [
+        { pattern: /bg-\[#(003B71|E97300|dce8f5|002a52|c96200)\]/ },
+        { pattern: /text-\[#(003B71|E97300|dce8f5|002a52|ffffff)\]/ },
+        { pattern: /border-\[#(003B71|E97300|dce8f5)\]/ },
+        { pattern: /hover:bg-\[#(003B71|E97300|dce8f5|002a52|c96200)\]/ },
+        { pattern: /hover:text-\[#(003B71|E97300|ffffff)\]/ },
+        'bg-white/10', 'bg-white/20', 'bg-white/30', 'bg-black/50',
+        'border-white/20', 'border-white/30',
+    ],
     theme: {
         extend: {},
     },
